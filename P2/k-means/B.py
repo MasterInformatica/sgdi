43dd6fb0f8f380d2e780c722ea2f1c3131cd8c06
friker92 <jesusjda@gmail.com>
2015-12-11 18:29:36 +0100

    actualizada coherencia promedio

--- P2/k-means/B.py
+++ P2/k-means/B.py
@@ -49,7 +49,7 @@ def media(lista):
 def coherencia_diametro(cluster):
     """
     Function: coherencia_diametro
-    Descrp: Calcula la coherencia de un claster por el
+    Descrp: Calcula la coherencia de un cluster por el
     metodo del diametro.
     Args:
     -> cluster: Lista de instancias que forman el cluster.
@@ -69,7 +69,7 @@ def coherencia_diametro(cluster):
 def coherencia_radio(cluster):
     """
     Function: coherencia_radio
-    Descrp: Calcula la coherencia de un claster por el
+    Descrp: Calcula la coherencia de un cluster por el
     metodo del radio.
     Args:
     -> cluster: Lista de instancias que forman el cluster.
@@ -82,23 +82,27 @@ def coherencia_radio(cluster):
     return max([distance.euclidean(i,c) for i in cluster])
 
 
-def coherencia_promedio(cluster):
+def coherencia_promedio(clustering):
     """
     Function: coherencia_promedio
-    Descrp: Calcula la coherencia de un claster por la
+    Descrp: Calcula la coherencia de un conjunto de clusters por la
     formula SUM(dist(c,i)^2)/N
     Args:
-    -> cluster: Lista de instancias que forman el cluster.
+    -> clustering: Lista de clusters.
     Return:
     -> Valor de coherencia
     """
 
-    c = A.get_centroide(cluster)
+
     suma = 0
-    for i in cluster:
-        d = distance.euclidean(c,i)
-        suma += d*d
-    return suma/(len(cluster)*1.0)
+    num = 0
+    for clu in clustering:
+        c = A.get_centroide(clustering[clu])
+        for i in clustering[clu]:
+            d = distance.euclidean(c,i)
+            suma += d*d
+            num +=1
+    return suma/(num*1.0)
 
 if __name__ == "__main__":
     instancias = A.read_file()
@@ -111,7 +115,7 @@ if __name__ == "__main__":
         res = A.kmeans(k, instancias)
         co_d.append(media([coherencia_diametro(res[0][c]) for c in res[0]]))
         co_r.append(media([coherencia_radio(res[0][c]) for c in res[0]]))
-        co_p.append(media([coherencia_promedio(res[0][c]) for c in res[0]]))
+        co_p.append(coherencia_promedio(res[0]))
     plot(K,co_d,"diametro")
     plot(K,co_r,"radio")
     plot(K,co_p,"promedio")
