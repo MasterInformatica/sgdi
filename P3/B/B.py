07ed5b9994843cc3acbafe37dbcd6644634558cb
lcostero <lcostero@ucm.es>
2016-01-16 21:53:24 +0100

    empezado el apartado A y B de la práctica

---
+++ P3/B/B.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+"""
+Autores: Luis M. Costero Valero & Jesús Doménech Arellano
+Grupo 04
+
+Este código es fruto ÚNICAMENTE del trabajo de sus miembros. Declaramos no 
+haber colaborado de ninguna manera con otros grupos, haber compartido el ćodigo 
+con otros ni haberlo obtenido de una fuente externa.
+"""
+
+from pymongo import MongoClient
+
+#################################################################
+## Es necesario añadir los parámetros adecuados a cada función ##
+#################################################################
+
+# 1. Añadir un usuario
+def insert_user():
+    pass
+
+
+# 2. Actualizar un usuario
+def update_user():
+    pass
+
+
+# 3. Añadir una pregunta
+def add_question():
+    pass
+
+
+# 4. Añadir una respuesta a una pregunta.
+def add_answer():
+    pass
+
+
+# 5. Comentar una respuesta.
+def add_comment():
+    pass
+
+
+# 6. Puntuar una respuesta.
+def score_answer():
+    pass
+
+
+# 7. Modificar una puntuacion de buena a mala o viceversa.
+def update_score():
+    pass
+
+
+# 8. Borrar una pregunta junto con todas sus respuestas, comentarios y 
+# puntuaciones
+def delete_question():
+    pass
+
+
+# 9. Visualizar una determinada pregunta junto con todas sus contestaciones
+# y comentarios. A su vez las contestaciones vendran acompañadas de su
+# numero de puntuaciones buenas y malas.
+def get_question():
+    pass
+
+
+# 10. Buscar preguntas con unos determinados tags y mostrar su titulo, su autor
+# y su numero de contestaciones.
+def get_question_by_tag():
+    pass
+
+
+# 11. Ver todas las preguntas o respuestas generadas por un determinado usuario.
+def get_entries_by_user():
+    pass
+
+
+# 12. Ver todas las puntuaciones de un determinado usuario ordenadas por 
+# fecha. Este listado debe contener el tıtulo de la pregunta original 
+# cuya respuesta se puntuo.
+def get_scores():
+    pass
+
+
+# 13. Ver todos los datos de un usuario.
+def get_user():
+    pass
+
+
+# 14. Obtener los alias de los usuarios expertos en un determinado tema.
+def get_uses_by_expertise():
+    pass
+
+
+# 15. Visualizar las n preguntas mas actuales ordenadas por fecha, incluyendo
+# el numero de contestaciones recibidas.
+def get_newest_questions():
+    pass
+
+
+# 16. Ver n preguntas sobre un determinado tema, ordenadas de mayor a menor por
+# numero de contestaciones recibidas.
+def get_questions_by_tag():
+    pass
+    
+    
+################################################################################
+############################  FUNCIONES AUXILIARES  ############################
+################################################################################
+
+# Incluir aqui el resto de funciones necesarias
+
+
+def inserta_usuarios(collection):
+    pass
+
+
+
+if __name__ == '__main__' : 
+
+    # Conexión con la base de datos
+    client = MongoClient('localhost',27017)
+    db = client['sgdi_grupo04']
+
+    #insertar los datos
+    # usuarios
+    usuarios = db['usuarios']
+    inserta_usuarios(usuarios)
+    
+    
+    
+
+
---
+++ P3/C/C.js
@@ -0,0 +1,64 @@
+/*******************************************************************************
+**************************** AGGREGATION FRAMEWORK *****************************
+*******************************************************************************/
+
+// Listado de pais-numero de usuarios ordenado de mayor a menor por numero de 
+// usuarios.
+function agg1(){
+  return "";
+}
+
+
+// Listado de pais-numero total de posts de los 3 paises con mayor numero total 
+// de posts, ordenado de mayor a menor por numero de posts.
+function agg2(){
+	return "";
+}
+
+  
+// Listado de aficion-numero de usuarios ordenado de mayor a menor numero de 
+// usuarios.
+function agg3(){
+	return "";
+}  
+  
+  
+// Listado de aficion-numero de usuarios restringido a usuarios espanoles y
+// ordenado de mayor a menor numero de usuarios.
+function agg4(){
+  return "";
+}
+
+
+
+/*******************************************************************************
+********************************** MAPREDUCE ***********************************
+*******************************************************************************/
+  
+// Listado de aficion-numero de usuarios restringido a usuarios espanoles.
+function mr1(){
+	return "";
+}
+
+
+// Listado de numero de aficiones-numero de usuarios, es decir, cuAntos
+// usuarios tienen 0 aficiones, cuantos una aficion, cuantos dos aficiones, etc.
+function mr2(){
+	return "";
+}
+
+
+// Listado de pais-numero de usuarios que tienen mas posts que contestaciones.
+function mr3(){
+	return "";
+}
+
+
+// Listado de pais-media de posts por usuario.
+function mr4(){
+	return "";
+}
+
+
+
+
